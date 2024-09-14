a544766106364129e99d3f598c0ffc3692065647
prachi16 <prachi@upforce.tech>
2024-09-14 16:26:27 +0530

    stop timer related changes push in main

---
+++ src/components/Stopwatch/FinalTimeDisplay.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FinalTimeDisplay = ({ finalTime }) => {
+  const formatTime = (time) => {
+    const milliseconds = `0${(time % 1000) / 10}`.slice(-2);
+    const seconds = `0${Math.floor((time / 1000) % 60)}`.slice(-2);
+    const minutes = `0${Math.floor((time / 60000) % 60)}`.slice(-2);
+    return `${minutes}:${seconds}:${milliseconds}`;
+  };
+  return <div className="final-time">Final Time: {formatTime(finalTime)}</div>;
+};
+
+export default FinalTimeDisplay;
